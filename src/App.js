26137e373d6b93dbf96ea03ac6c713582d0715ee
Andrei Fidelman <fidelmanlife@gmail.com>
2018-11-20 01:01:50 +0100

    Make Admin private

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component, Fragment } from 'react';
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import AdminPage from './components/routes/admin';
 import AuthPage from './components/routes/auth';
 import People from './components/routes/people';
+import PrivateRoute from './components/common/PrivateRoute';
+class App extends Component {
+  
 
-export default class App extends Component {
   render() {
+    const { auth: { user } } = this.props;
     return (
       <Fragment>
         <div>
-          <NavLink to="/admin" activeStyle={{ color: 'red' }}>
-            Admin
-          </NavLink>
+          {user && (
+            <NavLink to="/admin" activeStyle={{ color: 'red' }}>
+              Admin
+            </NavLink>
+          )}
         </div>
         <div>
           <NavLink to="/auth/sign-in" activeStyle={{ color: 'red' }}>
@@ -29,7 +35,7 @@ export default class App extends Component {
           </NavLink>
         </div>
         <div>
-          <Route path="/admin" component={AdminPage} />
+          <PrivateRoute isAuthenticated={user} path="/admin" component={AdminPage} />
           <Route path="/auth" component={AuthPage} />
           <Route path="/people" component={People} />
         </div>
@@ -37,3 +43,61 @@ export default class App extends Component {
     );
   }
 }
+
+export default withRouter(connect((state) => {
+  const { auth } = state;
+  return { auth };
+})(App));
+
+
+// import React, { Component, Fragment } from 'react';
+// import { NavLink, Route, Redirect } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import AdminPage from './components/routes/admin';
+// import AuthPage from './components/routes/auth';
+// import People from './components/routes/people';
+
+// class App extends Component {
+//   render() {
+//     const PrivateRoute = ({ component: Component, ...rest }) => (
+//       <Route {...rest} render={(props) => (
+//          this.props.user
+//           ? <Component {...props} />
+//           : <Redirect to='/' />
+//       )} />
+//     )
+
+//     return (
+//       <Fragment>
+//         <div>
+//           <NavLink to="/admin" activeStyle={{ color: 'red' }}>
+//             Admin
+//           </NavLink>
+//         </div>
+//         <div>
+//           <NavLink to="/auth/sign-in" activeStyle={{ color: 'red' }}>
+//             Sign In
+//           </NavLink>
+//         </div>
+//         <div>
+//           <NavLink to="/auth/sign-up" activeStyle={{ color: 'red' }}>
+//             Sign Up
+//           </NavLink>
+//         </div>
+//         <div>
+//           <NavLink to="/people/add" activeStyle={{ color: 'red' }}>
+//             Add People
+//           </NavLink>
+//         </div>
+//         <div>
+//           {/* <PrivateRoute path='/admin' component={AdminPage} /> */}
+//           <Route path="/auth" component={AuthPage} />
+//           <Route path="/people" component={People} />
+//         </div>
+//       </Fragment>
+//     );
+//   }
+// }
+
+
+
