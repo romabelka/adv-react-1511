cb1aca3ca3a6009bb2768d6653b870d95c32d8c7
maxbabarov <mbabarov@gmail.com>
2018-11-25 15:13:46 +0200

    update firebase config

--- src/config.js
+++ src/config.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase/app'
+import 'firebase/auth'
 
 export const appName = 'advreact-15-11'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDWW03UZbUlsONRLXicKn0GEVz49sQF9kU',
-  authDomain: `${appName}.firebaseapp.com`,
-  databaseURL: `https://${appName}.firebaseio.com`,
-  projectId: appName,
-  storageBucket: `${appName}.appspot.com`,
-  messagingSenderId: '509870743520'
+  apiKey: 'AIzaSyCagGg_3tIdTjzSkdMChi1rfZ4F5qTKPMw',
+  authDomain: 'advanced-react-app.firebaseapp.com',
+  databaseURL: 'https://advanced-react-app.firebaseio.com',
+  projectId: 'advanced-react-app',
+  storageBucket: 'advanced-react-app.appspot.com',
+  messagingSenderId: '93649441415'
 }
 
 firebase.initializeApp(firebaseConfig)
