5a608b20223b219e64ad2f69981e56fddc448bcc
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2018-11-27 10:52:11 +0200

    HT2 :: add my firebase

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app'
 
-export const appName = 'advreact-15-11'
+export const appName = 'course-react-advanced'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDWW03UZbUlsONRLXicKn0GEVz49sQF9kU',
+  apiKey: 'AIzaSyCpt1zg3i1Ge4A5hwRFGf-LH2bL0Ktp2Ko',
   authDomain: `${appName}.firebaseapp.com`,
   databaseURL: `https://${appName}.firebaseio.com`,
   projectId: appName,
   storageBucket: `${appName}.appspot.com`,
-  messagingSenderId: '509870743520'
+  messagingSenderId: '555325786267'
 }
 
 firebase.initializeApp(firebaseConfig)
