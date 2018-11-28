95f8f72ca169a446041bf1881d8f8d38a4f44beb
nicolaivoronin <nicolai.voronin@gmail.com>
2018-11-28 17:43:05 +0200

    Rewrite selected events to virtualized list

--- src/components/events/selected-events.js
+++ src/components/events/selected-events.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Table, Column } from 'react-virtualized'
+
 import { selectedEventsSelector } from '../../ducks/events'
 import SelectedEventCard from './selected-event-card'
 
@@ -10,12 +12,21 @@ class SelectedEvents extends Component {
     console.log('---', this.props.events)
     return (
       <div>
-        {this.props.events.map((event) => (
-          <SelectedEventCard event={event} key={event.id} />
-        ))}
+        <Table
+          height={250}
+          width={700}
+          rowCount={this.props.events.length}
+          rowHeight={30}
+          rowGetter={this.rowGetter}
+        >
+          <Column width={200} dataKey="title" label="Title" />
+          <Column width={200} dataKey="where" label="Where" />
+        </Table>
       </div>
     )
   }
+
+  rowGetter = ({ index }) => this.props.events[index]
 }
 
 export default connect((state) => ({
