02e8a320804882db6945d167e9b6b03cf31f98c3
valentin <vip7@ukr.net>
2018-12-12 00:17:37 +0200

    ht6.1, ht6.2

--- mobile/App.js
+++ mobile/App.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, ScrollView } from 'react-native'
 //import HelloWorld from './components/hello-world';
 import Auth from './components/auth'
 import { events } from './fixtures'
 import EventList from "./components/event-list";
+import EventsSectionList from './components/event-section-list'
 
 const eventList = Object.entries(events).map(([ id, event ]) => ({ id, ...event }))
 
 export default class App extends React.Component {
+
+  state = {
+    newEventList : false,
+  }
+
+  toggleEventList = () => {
+    this.setState({newEventList: !this.state.newEventList})
+  }
+
   render() {
+
+    const title = this.state.newEventList ? 'close events list' : 'open events list'
+
     return (
+      <ScrollView>
+        <View style={{paddingTop: 50}}>
+          <Button
+            style={{
+              fontSize: 20
+            }}
+            onPress={this.toggleEventList}
+            title={title}
+          />
+        </View>
       <View style={styles.container}>
-        <EventList events = {eventList} />
+
+          {this.state.newEventList && <EventsSectionList events = {eventList}/> }
       </View>
+      </ScrollView>
     );
   }
 }
