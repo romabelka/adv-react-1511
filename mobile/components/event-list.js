28df9874a69ecb9f4589ba142460cac5dfb3b8b3
SeaSalt88 <seasalt88@gmail.com>
2018-12-12 17:12:14 +0100

    HT6.2 Tried to add shadows

--- mobile/components/event-list.js
+++ mobile/components/event-list.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {View, ScrollView, Text, StyleSheet, SectionList} from 'react-native'
+import {BoxShadow} from 'react-native-shadow'
 import _  from 'lodash'
 
 class EventList extends Component {
@@ -21,11 +22,17 @@ class EventList extends Component {
     render() {
         return (
             <ScrollView style={styles.container}>
-                <View style={styles.sectionContainer}>
+                <View style={styles.sectionsContainer}>
                     <SectionList
-                        renderItem={({item, index, section}) => <Text key={index}>{item.title}</Text>}
-                        renderSectionHeader={({section: {title, data}}) => (
-                            <Text style={{fontWeight: 'bold'}}>{title} count: {data.length}</Text>
+                        renderItem={({item, index, section}) => (
+                                    /*<BoxShadow setting={shadowOpt}>*/
+                                        <View style={styles.sectionItem}>
+                                            <Text key={index}>{item.title}</Text>
+                                        </View>
+                                    /*</BoxShadow>*/
+                        )}
+                        renderSectionHeader={({section: { data, title }}) => (
+                            <View style={styles.sectionHeader}><Text style={{fontWeight: 'bold'}}>{title} count: {data.length}</Text></View>
                         )}
                         sections={this.getSections()}
                         keyExtractor={(item, index) => item + index}
@@ -43,11 +50,44 @@ const styles = StyleSheet.create({
         paddingRight: 20,
         paddingLeft: 20
     },
-    sectionContainer: {
+    sectionsContainer: {
         backgroundColor: "white",
-        padding: 20,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
         marginBottom: 80
+    },
+    sectionHeader: {
+        marginTop: 20,
+        marginBottom: 10
+    },
+    sectionItem: {
+        paddingTop: 5,
+        paddingBottom: 5,
+        marginTop: 5,
+        marginBottom: 5,
+        backgroundColor: 'white',
+        shadowColor:'red',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 5,
+        shadowOpacity: 1.0,
+        elevation: 5
     }
 })
+
+const shadowOpt = {
+	width:500,
+	height:30,
+	color:"#FFFF00",
+	border:0,
+	radius:0,
+	opacity:0.2,
+	x:0,
+    y:0
+}
+
 
 export default EventList
